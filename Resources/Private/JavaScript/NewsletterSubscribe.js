080b2de58f3dbf96412d7d8ed9b4c436ef79019d
Gregor Agnes <ga@zwo3.de>
2021-03-08 09:45:27 +0100

    Fix for undefined function

--- Resources/Private/JavaScript/NewsletterSubscribe.js
+++ Resources/Private/JavaScript/NewsletterSubscribe.js
@@ -43,7 +43,7 @@ let insertNoSpamBotField = () => {
   document.getElementById('NewsletterSubscribeSubmit').before(noSpamField)
 }
 
-if (iAmNotASpamBotValue) {
+if (typeof (iAmNotASpamBotValue) !== "undefined") {
   document.addEventListener("DOMContentLoaded", () => {
     insertNoSpamBotField()
   })
