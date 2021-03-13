d1f00381e86cbb5463212108eed1d054ec901773
Greg-Think <ga@zwo3.de>
2021-03-13 11:58:24 +0100

    fix ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,20 @@
 <?php
 
-$EM_CONF['newsletter_subscribe'] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Newsletter Subscribe',
 	'description' => 'subscribe and unsubscribe to tt_address, generate static Link to unsubscribe (to use in Newsletter)',
 	'category' => 'plugin',
 	'author' => 'Gregor Agnes',
 	'author_email' => 'ga@zwo3.de',
 	'author_company' => 'Gregor Agnes & Markus Cousin GbR',
-	'shy' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '1',
-	'createDirs' => '',
-	'modify_tables' => '',
+	'state' => 'stable',
 	'clearCacheOnLoad' => 0,
-	'lockType' => '',
 	'version' => '3.2.1',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => [
+		'depends' => [
 			'typo3' => '10.4.2-10.4.99',
             'tt_address' => '4.0.0-5.99.99'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+        ],
+    ],
+];
 
