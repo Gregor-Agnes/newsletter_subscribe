fc2a1c9604face266665983d76d4016fca8f3e71
Igor Neyman <in@points.de>
2021-06-03 15:48:52 +0200

    add plugins to wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,27 +1,74 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'NewsletterSubscribe',
-    'Subscribe',
-    [
-        \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showForm, createConfirmation, undosubscribe, unsubscribe, doConfirm, createUnsubscribeMail',
-    ],
-    // non-cacheable actions
-    [
-        \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showForm, createConfirmation, undosubscribe, unsubscribe, doConfirm, createUnsubscribeMail',
-    ]
-);
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'NewsletterSubscribe',
-    'Unsubscribe',
-    [
-        \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showUnsubscribeForm, unsubscribe, createUnsubscribeMail',
-    ],
-    // non-cacheable actions
-    [
-        \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showUnsubscribeForm, unsubscribe, createUnsubscribeMail',
-    ]
+call_user_func(
+    function()
+    {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'NewsletterSubscribe',
+            'Subscribe',
+            [
+                \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showForm, createConfirmation, undosubscribe, unsubscribe, doConfirm, createUnsubscribeMail',
+            ],
+            // non-cacheable actions
+            [
+                \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showForm, createConfirmation, undosubscribe, unsubscribe, doConfirm, createUnsubscribeMail',
+            ]
+        );
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'NewsletterSubscribe',
+            'Unsubscribe',
+            [
+                \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showUnsubscribeForm, unsubscribe, createUnsubscribeMail',
+            ],
+            // non-cacheable actions
+            [
+                \Zwo3\NewsletterSubscribe\Controller\SubscribeController::class => 'showUnsubscribeForm, unsubscribe, createUnsubscribeMail',
+            ]
+        );
+
+        // wizards
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            'mod {
+                wizards.newContentElement.wizardItems.plugins {
+                    elements {
+                        subscribe {
+                            iconIdentifier = zwo3_newslettersubscribe-plugin-subscribe
+                            title = LLL:EXT:newsletter_subscribe/Resources/Private/Language/locallang_db.xlf:tx_zwo3_newslettersubscribe_subscribe.name
+                            description = LLL:EXT:newsletter_subscribe/Resources/Private/Language/locallang_db.xlf:tx_zwo3_newslettersubscribe_subscribe.description
+                            tt_content_defValues {
+                                CType = list
+                                list_type = newslettersubscribe_subscribe
+                            }
+                        }
+                        unsubscribe {
+                            iconIdentifier = zwo3_newslettersubscribe-plugin-unsubscribe
+                            title = LLL:EXT:newsletter_subscribe/Resources/Private/Language/locallang_db.xlf:tx_zwo3_newslettersubscribe_unsubscribe.name
+                            description = LLL:EXT:newsletter_subscribe/Resources/Private/Language/locallang_db.xlf:tx_zwo3_newslettersubscribe_unsubscribe.description
+                            tt_content_defValues {
+                                CType = list
+                                list_type = newslettersubscribe_unsubscribe
+                            }
+                        }
+                    }
+
+                    show = *
+                }
+            }'
+        );
+
+        // icons
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+
+        foreach(['subscribe', 'unsubscribe'] as $pluginName) {
+            $iconRegistry->registerIcon(
+                'zwo3_newslettersubscribe-plugin-'.$pluginName,
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                ['source' => 'EXT:newsletter_subscribe/Resources/Public/Gfx/Extension.svg']
+            );
+        }
+
+    }
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_newslettersubscribe_subscribe[subscriptionHash]';
