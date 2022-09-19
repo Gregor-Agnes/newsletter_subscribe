f6526d11fd8db4a2a9a3d6b1e4422ea5d8224742
Gregor <ga@zwo3.de>
2022-09-19 13:54:09 +0200

    feat: drop IE compatability

--- Resources/Private/JavaScript/NewsletterSubscribe.js
+++ Resources/Private/JavaScript/NewsletterSubscribe.js
@@ -1,31 +1,5 @@
 import '../Scss/Styles.scss'
 
-// Polyfill IE
-// from: https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/before()/before().md
-(function (arr) {
-  arr.forEach(function (item) {
-    if (item.hasOwnProperty('before')) {
-      return;
-    }
-    Object.defineProperty(item, 'before', {
-      configurable: true,
-      enumerable: true,
-      writable: true,
-      value: function before() {
-        var argArr = Array.prototype.slice.call(arguments),
-          docFrag = document.createDocumentFragment();
-
-        argArr.forEach(function (argItem) {
-          var isNode = argItem instanceof Node;
-          docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
-        });
-
-        this.parentNode.insertBefore(docFrag, this);
-      }
-    });
-  });
-})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
-
 const iAmNotASpamBotContainer = document.getElementById('iAmNotASpamBotContainer');
 const iAmNotASpamBotValue = (iAmNotASpamBotContainer !== null && iAmNotASpamBotContainer.dataset.iamnotaspambotvalue) ? iAmNotASpamBotContainer.dataset.iamnotaspambotvalue : undefined;
 const iAmNotARobotLabel = (iAmNotASpamBotContainer !== null && iAmNotASpamBotContainer.dataset.iamnotarobotlabel) ? iAmNotASpamBotContainer.dataset.iamnotarobotlabel : undefined;
@@ -38,7 +12,7 @@ let insertNoSpamBotField = () => {
 <label class="label checkbox" for="iAmNotASpamBotHere">
     ${iAmNotARobotLabel}
 </label>
-                
+
 <input type="hidden" name="iAmNotASpamBot" value="">
 <label class="label" for="iAmNotASpamBot">
 <input id="iAmNotASpamBot" type="checkbox" name="iAmNotASpamBot" value="${iAmNotASpamBotValue}">
