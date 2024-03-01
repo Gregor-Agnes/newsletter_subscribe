c13fdb7072ee2e7921320a5a851de0fe722ef682
Gregor <ga@zwo3.de>
2024-03-01 16:31:56 +0100

    Add new Spambot middleware to newsletter_subscribe package
    A new middleware, Spambot, has been added to the newsletter_subscribe package. This ensures basic spam prevention mechanism by checking if 'iAmNotASpamBot' is set and matches the stored session value. It then modifies the request accordingly, aiding in spam prevention efforts.

---
+++ Classes/Middleware/Spambot.php
@@ -0,0 +1,28 @@
+<?php
+namespace Zwo3\NewsletterSubscribe\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class Spambot implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $backendConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('newsletter_subscribe');
+        $useSimpleSpamPrevention = (bool)($backendConfiguration['useSimpleSpamPrevention'] ?? false);
+        if ($useSimpleSpamPrevention)
+        {
+            $iAmNotASpamBot = $request->getParsedBody()['iAmNotASpamBot'] ?? null;
+            if ($iAmNotASpamBot !== null && $iAmNotASpamBot != $GLOBALS['TSFE']->fe_user->getKey('ses', 'i_am_not_a_robot'))
+            {
+                $request = $request->withAttribute('spambotFailed', true);
+            }
+        }
+        
+        return $handler->handle($request);
+    }
+}
