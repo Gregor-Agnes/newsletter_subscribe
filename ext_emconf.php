36a46fedb16129fb8e28f632440921b1fc5654df
Gregor <ga@zwo3.de>
2024-10-25 16:06:50 +0200

    refactor: rename array key and update dependencies
    - Renamed array key from $_EXTKEY to 'newsletter_subscribe'.
    - Updated version to 7.0.0.
    - Extended TYPO3 dependency to include versions up to 13.4.99.
    - Adjusted tt_address dependency range to 8.0.0 - 9.99.99.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['newsletter_subscribe'] = [
     'title' => 'Newsletter Subscribe',
     'description' => 'subscribe and unsubscribe to tt_address, generate static Link to unsubscribe (to use in Newsletter), remove unvalidated subscriptions with scheduler task',
     'category' => 'plugin',
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Gregor Agnes & Markus Cousin GbR',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '6.2.0',
+    'version' => '7.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.00-12.4.99',
-            'tt_address' => '8.0.0'
+            'typo3' => '12.4.00-13.4.99',
+            'tt_address' => '8.0.0 - 9.99.99',
         ],
         'conflicts' => [
         ],
